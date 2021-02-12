e40e100a540745b9bdaf152dbaa43d5881f7bf2b
Khaled Riyad <khaledriyad57@gmail.com>
2021-02-12 22:47:05 +0200

    ISA 5eer

--- Final.asm
+++ Final.asm
@@ -11,7 +11,13 @@
 	tirm: .asciiz "8 - For Exit\n"
 	limitex: .asciiz "Size Limit Exceeded"
 	done: .asciiz "Done!"
+	enterreq: .asciiz "Enter Request Name: "
+	enterpir: .asciiz "Enter Request Priority: "
+	message1 :  .asciiz  "Request :"
+	message2 : .asciiz  "\nPriority: :"
 	
+	name: .byte
+	priority: .word
 	choice: .word 8
 	
 	list1 : .space 160 
@@ -72,9 +78,9 @@ whilo:
 
 	choice1:
 		bne $t0,1,choice2
-		li $v0,4
-		la $a0,addr
-		syscall
+		
+        jal add_request    
+            
 		j whilo
 	choice2:
 		bne $t0,2,choice3
@@ -125,29 +131,154 @@ exit:
 
 
 
-get_size :   			# Get Size Function
-	la $t8 , ($a0)
-	addi $t0 , $0 , 0
-	addi $t1 , $0 , 0
-	addi $t2 , $0 , 20
-	addi $t3 , $0 , 1
-	addi $t5 , $0 , 0 
-for :
-	slt $t4 , $t1 , $t2
-	bne $t4 , $t3 , Endfor
+add_request:
+	li $v0,4
+	la $a0,enterpir
+	syscall
 	
-	sll $t7 , $t1 , 3   # t7 = 8 * i 
-	add $t7 , $t7 , $t8
-	lw  $t9 , 0($t7)
+	
+    li $v0, 5 # read the priority
+    syscall
+    move $t0, $v0
+	
+	li $v0,1
+	move $a0,$t0
+	syscall
 
-	beq  $t9 , $t5 , endif
-	addi $t0 , $t0 , 1
+
+	li $v0,4
+	la $a0,enterreq
+	syscall
+		
+    li $v0, 12		# read the character
+    syscall
+    move $t4,$v0
 	
-endif :
-	addi $t1 , $t1 , 1
-	j for
-Endfor :
-	addi $v0 , $t0 , 0 
+	li $v0,11
+	move $a0,$t4
+	syscall
+	
+     
+    la  $s1 , list1
+	la  $s2 , list2
+	la  $s3 , list3
+	la  $s4 , list4	    
+       
+	la $a0 , ($s1)    	# Load list1 address
+	jal get_size   	 	# call list1 size
+	li $t2, 20      	# max size 20
+	move $t1, $v0   	# store list1 size
+
+	li $v0,1
+	move $a0,$t1
+	syscall 
+	
+	bgt  $t1 , $t2, else_if_1
+while_1:
+	li $t3,0         	# int i = 0
+	sll $t7 , $t3 , 3 	# t7 = 8 * i
+	add $t7 , $t7 , $s1
+	lw $t9 , 0($t7)
+
+ 	beq $t9 , $0 , end_while_1
+	addi $t3 , $t3 , 1
+	j while_1
+	
+	end_while_1 :
+	sw $t0, 0($t7)  # store priority
+	sb $t4, 4($t7)  # store name
+	j exit_add
+	
+else_if_1:
+	la $a0 ,($s2)    	# Load list2 address
+	jal get_size   	 	# call list2 size
+	li $t2, 20      	# max size 20
+	move $t1, $v0   	# store list2 size
+	bgt  $t1 , $t2, else_if_2
+while_2:
+	li $t3,0         	# int i = 0
+	sll $t7 , $t3 , 3 	# t7 = 8 * i
+	add $t7 , $t7 , $s2
+	lw $t9 , 0($t7)
+
+ 	beq $t9 , $0 , end_while_2
+	addi $t3 , $t3 , 1
+	j while_2
+	
+	end_while_2 :
+	sw $t0, 0($t7)  # store priority
+	sb $t4, 4($t7)  # store name
+	j exit_add
+	
+	
+	
+else_if_2:
+	la $a0 , ($s3)    	# Load list3 address
+	jal get_size   	 	# call list3 size
+	li $t2, 20      	# max size 20
+	move $t1, $v0   	# store list2 size
+	bgt  $t1 , $t2, else_if_3
+while_3:
+	li $t3,0         	# int i = 0
+	sll $t7 , $t3 , 3 	# t7 = 8 * i
+	add $t7 , $t7 , $s3
+	lw $t9 , 0($t7)
+
+ 	beq $t9 , $0 , end_while_3
+	addi $t3 , $t3 , 1
+	j while_3
+	
+	end_while_3 :
+	sw $t0, 0($t7)  # store priority
+	sb $t4, 4($t7)  # store name
+	j exit_add
+	
+	
+else_if_3:
+	la $a0 , ($s4)    	# Load list4 address
+	jal get_size   	 	# call list4 size
+	li $t2, 20      	# max size 20
+	move $t1, $v0   	# store list2 size
+	bgt  $t1 , $t2, exit_add
+while_4:
+	li $t3,0         	# int i = 0
+	sll $t7 , $t3 , 3 	# t7 = 8 * i
+	add $t7 , $t7 , $s4
+	lw $t9 , 0($t7)
+
+ 	beq $t9 , $0 , end_while_4
+	addi $t3 , $t3 , 1
+	j while_4
+	
+	end_while_4 :
+	sw $t0, 0($t7)  # store priority
+	sb $t4, 4($t7)  # store name
+	j exit_add	
+	
+exit_add:
+	jr $ra
+
+
+
+get_size :   				# Get Size Function returns the number of elements in an array
+        addi   $sp,$sp,-8
+        sw  $ra,0($sp)
+        sw  $a0,4($sp)
+        li  $t1,0
+
+Whileo:
+        lw  $t2,0($a0)
+        beq $t2,$0,endo
+        addi    $t1,$t1,1
+        addi    $a0,$a0,4
+        j   Whileo
+
+endo:    
+        move $v0,$t1
+        lw  $ra,0($sp)
+        lw  $a0,4($sp)
+        addi    $sp,$sp,8
+        jr  $ra
 
 
 
@@ -299,7 +430,9 @@ swap:               	#swap method
 	addi $sp, $sp, 12   #Restoring the stack size
 	jr $ra          	#jump back to the caller
 
-	partition:          #partition method
+
+
+partition:          #partition method
 
 	addi $sp, $sp, -16  #Make room for 5
 
@@ -353,6 +486,7 @@ endfor:
     jr $ra              #junp back to the caller
 
 
+
 quick_sort:
 	addi $sp, $sp, -16      # Make room for 4
 
@@ -385,6 +519,7 @@ endif0:
 	lw $ra, 12($sp)         #restore return address
 	addi $sp, $sp, 16       #restore the stack
 	jr $ra                  #return to caller 
+	
 	
 	
 put_all : 				# put all function
@@ -573,6 +708,116 @@ update_priority:              		#void update_priority(char name, int priority)
     	syscall
    	jr   $ra
    	
+   	
+binary_search: #void binary_search(int l, int r, char name)
+move $s7 , $ra
+addiu $sp, $sp, -24  #6 room in memory       
+sw   $a0, 0($sp) #i
+sw   $a1, 4($sp) #r
+sw   $a2, 8($sp) #name
+sw   $s5 , 12($sp)
+sw   $s6 , 16($sp)
+sw   $ra, 20($sp) #return address
+
+la  $s0 , all
+la  $s1 , list1
+la  $s2 , list2
+la  $s3 , list3
+la  $s4 , list4	
+
+#put_all function 
+jal put_all # call put_all()
+jr $ra
+#quick_sort function
+addi $s5 , $0 , 0 #$s5 = 0
+addi $s6 , $0 , 0 #$s6 = 0
+la $a0 , 0($s1)
+jal get_size #(get_size(list1)
+jr $ra#ignor overloading
+move $s5 , $v0#add fiest return value of get size of list1 to $s5
+
+
+la $a0 , 0($s2)
+jal get_size #(get_size(list2)
+jr $ra#ignor overloading
+add $s5 , $s5 , $v0 #add new return value of get size of list2 to $s5
+la $a0 , 0($s3)
+jal get_size #(get_size(list3)
+jr $ra#ignor overloading
+add $s5 , $s5 , $v0 #add new return value of get size of list3 to $s5
+
+
+la $a0 , 0($s3)
+jal get_size #(get_size(list4)
+jr $ra #ignor overloading
+
+add $s5 , $s5 , $v0 #add new return value of get size of list4 to $s5
+addi $t4 , $t4 , 1 #make t4 =  1 to subtract from s5 in the next step
+sub $s5 , $s5 , $t4 #(get_size(list1) + get_size(list2) + get_size(list3) + get_size(list4)) - 1 (second parameter in quick sort)
+addi $t0 ,$0 , 0 #(first parameter in quick sort = 0) 
+
+move $a0 , $t0 #send first parameter to quick sort
+move $a1 , $s5 #send second parameter to quick sort
+jal  quick_sort # call quick_sort
+jr $ra #ignor overloading
+# first if statment
+blt $a1,$a0,Exit # if (r < l) go to exit 
+F1:
+addi $t0, $0,0 #mid = 0 
+addi $t5 , $t5 , 1 #make t5 = 1 to subtract from r in the next step
+sub $t1 ,$a1 , $t5 # r-1
+addi $t2 , $0 , 2 #2
+div $t3 , $t1 , $t2 #(r-1)/2
+add $t0 , $a0 , $t3 #i + (r-1)/2
+
+#second if statment  if (all[mid].name == name)
+sll $s0 , $t0, 3 #all = 8 * mid
+add $s0, $s0 , $s5
+lw $t6 , 4($s0)#need to know access 
+bne $t6 , $0 , Exit_if1 #if not equal
+
+li $v0 , 4
+la $a0 , message1
+syscall
+#print all[mid].name bas ana me4 3aref :"
+
+li $v0 , 4
+la $a0 , message2
+syscall
+#print all[mid].priority bas ana bardo me4 3aref :(
+
+Exit_if1:#if not equal
+#if (all[mid].name > name)
+sll $s0 , $t0 , 3 #all = 8 * mid
+add $s0 , $s0 , $s6
+lw  $t7, 4($s0)#need to know access 
+blt $t7 , $0 , Exit_if2 #less than
+#return statment
+addi $t4 , $t4 , 1 #t4 = 1
+sub $t0 , $t0 , $t4 #mid - 1
+move $a1 , $t0 
+jal binary_search 
+jr $s7 #to back to this register again 
+
+
+Exit_if2:#if less than
+#second return statment 
+li $t4 , 1 #t4 = 1
+add $t0 , $t0 , $t4 #mid + 1
+move $a0 , $t0 
+jal binary_search 
+jr $s7 #to back to this register again 
+  
+Exit0:
+lw $a0 , 0($sp)#restor
+lw $a1 , 4($sp)#restor
+lw $a2 , 8($sp)#restor
+lw $s5 , 12($sp)#restor
+lw $s6 , 16($sp)#restor
+lw $ra , 20($sp)#restor
+addi $sp , $sp , 24 #add 24 in stack pointer
+jr $ra 
+
    	
 proccess_all: # process all
 
